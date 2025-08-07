a3624da9450b9de0d4f4e5f249e25cf860849c81
landn.thrn <landon.thorne48@gmail.com>
2025-08-06 22:07:18 -0700

    Add files via upload

---
+++ Matrix Effect on Startup by landn.thrn/Matrix 20 Tabs.bat
@@ -0,0 +1,18 @@
+@echo off
+title Entering the Matrix...
+color 0a
+
+:: Number of tabs to open
+set "count=20"
+
+:: Path to your main Matrix batch file
+set "matrixPath=C:\Windows\Batch Files\Entering the Matrix Startup.bat"
+
+:: Delay between tab launches (in seconds)
+set "delay=0.4"
+
+:: Loop and launch
+for /L %%i in (1,1,%count%) do (
+    start "" "%matrixPath%"
+    powershell -Command "Start-Sleep -Seconds %delay%"
+)
